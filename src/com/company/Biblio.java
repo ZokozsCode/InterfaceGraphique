4d9f21df4e172d0c27af6cce95d708a08a16ac5c
alexismajchrzak <alexis.majchrzak77@gmail.com>
2020-12-16 15:24:14 +0100

    Update Biblio.java
    JPanel + JMenubar

--- src/com/company/Biblio.java
+++ src/com/company/Biblio.java
@@ -1,12 +1,20 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Biblio extends JFrame {
 
     public Biblio(){
          super("Bibliotheque");
-         setSize(500,450);
+         setSize(500,500);
+
+         JPanel monPanel = new JPanel();
+         this.setContentPane(monPanel);
+         monPanel.setBackground(Color.LIGHT_GRAY);
+
+         JMenuBar monMenuBarr = new JMenuBar();
+         this.setJMenuBar(monMenuBarr);
 
 
 
